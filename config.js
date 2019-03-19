2ff15ded838029db3bbf9cc14d70f88646c44f1f
WDelenclos <wdelenclos@gmail.com>
2019-03-19 11:26:01 +0100

    [Add] Basic request with ozae

---
+++ config.js
@@ -0,0 +1,12 @@
+/**
+ * Configuration
+ * @type {{config: {mode: string, ozae: {apiKey: string}, port: number}}}
+ */
+module.exports = {
+        // MODES : string ( dev || prod )
+        mode: "dev",
+        ozae: {
+            apiKey: "9ea16bdd81fd42f2aaaa6c640f1ababd"
+        },
+        port: 8060,
+};
